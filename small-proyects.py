97b3d7e36393f74a757fafb7d0ab9c04946d7a82
Brayan pujols <brayanpujol100@gmail.com>
2026-06-17 00:41:01 -0400

    rename a directory

---
+++ small-proyects.py
@@ -0,0 +1,8 @@
+
+print (""" hello client this is the best coffeshop, but tell me how many coffe do you like today this is the menu, we have normal coffe for 24.02$ and express coffe for 30.01 """)
+
+normal = 24.02 
+express = 30.01
+
+client = input ("how many you want?")
+
