0e6316355d958ee3f3cbf9ef75276d7adae7f3fa
Brayan pujols <brayanpujol100@gmail.com>
2026-06-20 17:17:50 -0400

    that is several modifition's scripts and the creation of backups

--- scripst/eldenring.sh
+++ scripst/eldenring.sh
@@ -5,19 +5,19 @@ echo "select your clash if you wanna continua 1- samuro 2-pariguayo 3-el medio g
 read class
 
 case  $class in 
-      1)    type="samuro"
+      1)    type="samurai"
             hp=20
             power=15
             ;;
 
-      2)    type="pariguayo"
+      2)    type="payaso"
             hp=14
             power=45
             ;;
   
-      3)    type="el medio gay"
-            hp=18
-            power=3
+      3)    type="el medio muerto"
+            hp=1
+            power=32
             ;;
 esac 
 
---
+++ scripst/small-proyects.py
@@ -0,0 +1,7 @@
+print ("hello everyone can you say me what you wanna drink today")
+
+coffe = float(input("how many coffe you want"))
+price = 25.23
+total = coffe * price 
+
+print (f" this is the total $ {total} dollars ") 
